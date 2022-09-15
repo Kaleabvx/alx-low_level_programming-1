6682c534797fe0684b5dfd1c9469722fb17a6ffd
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-09-15 07:20:43 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_isupper -> a function to check upper case alpha
+*@c: an integer paramerr
+*Return: something but success
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}

de1219fdc9f5918b8d3de97c45b5f1f07889dbda
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-09-22 12:08:29 -0700

    Task 5 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* string_toupper -> converting any string to upperCase
+* @x: string param x
+* Return: string ..
+*/
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}

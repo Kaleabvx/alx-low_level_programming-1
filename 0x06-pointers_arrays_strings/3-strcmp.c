e51a17a0dc7771e1f919e42b1ca78569e5a41a7b
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-09-22 12:00:57 -0700

    Task 3 created

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strcmp -> for comparing purpose
+* @s1: paramFirst
+* @s2: paramSecond
+* Return: integer of funnc
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}

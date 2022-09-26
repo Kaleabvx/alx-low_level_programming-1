444b63dedc76c92656b4cf21ba673395d4e6248c
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-09-26 13:16:01 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - It Searches a string for any of a set of bytes.
+ * @s: The string In the code must be searched.
+ * @accept: The set of bytes
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}
